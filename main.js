6b78414725da78359b70297c8890a77cae8c838b
podmar <marta.podziewska@gmail.com>
2022-04-22 10:25:39 +0200

    fixed the bug loading the table entries

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 //-----TABLE OF CONTENTS-----
-//----->"read more" button on the main page
-//----->Table with data on the street-festivals.html page
-
+//->"read more" button on the main page
+//->fetching data & creating cards & table
+//->display cards function for the main page
+//->display table function for the street-festivals.html page
 
 
 //-----END OF TABLE OF CONTENTS-----
-// fetching data from the data file
-let festivalData = data; 
 
 //----->"read more" button on the main page
 //#region
@@ -36,57 +35,77 @@ function buttonClickAction () {
 };
 
 //#endregion
-//----->Festival display cards on the main page
+//----->fetching data & creating cards & table
 //#region
-let cardContainer = document.getElementById("card-container");
+let festivalData = data; 
 
-for (let i = 0; i < 3; i++) {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-    let cardBody = document.createElement("div");
-    cardBody.setAttribute("class", "card-body")
-    let cardTitle = document.createElement("h5");
-    cardTitle.setAttribute("class", "card-title");
-    cardTitle.innerText = festivalData[0]["index"][i]["bezeichnung"]
-    let cardText = document.createElement("p");
-    cardText.setAttribute("class", "card-text");
-    let linkLocation = festivalData[0]["index"][i]["www"];
-    let eventLink = document.createElement("a");
-    eventLink.setAttribute("href", linkLocation);
-    eventLink.innerHTML=linkLocation;
-    cardText.innerText = `${festivalData[0]["index"][i]["strasse"]}, on ${festivalData[0]["index"][i]["von"]}, more info on `;
+displayCards();
+displayTable();
+//#endregion
+//----->display cards function for the main page
+//#region
 
-    cardText.appendChild(eventLink);
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    card.appendChild(cardBody);
-    cardContainer.appendChild(card);
+function displayCards () {
+    let cardContainer = document.getElementById("card-container");
+
+    //validating if user on the main page
+    if (cardContainer != null) {
+        //generating cards
+        for (let i = 0; i < 3; i++) {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        let cardBody = document.createElement("div");
+        cardBody.setAttribute("class", "card-body")
+        let cardTitle = document.createElement("h5");
+        cardTitle.setAttribute("class", "card-title");
+        cardTitle.innerText = festivalData[0]["index"][i]["bezeichnung"]
+        let cardText = document.createElement("p");
+        cardText.setAttribute("class", "card-text");
+        let linkLocation = festivalData[0]["index"][i]["www"];
+        let eventLink = document.createElement("a");
+        eventLink.setAttribute("href", linkLocation);
+        eventLink.innerHTML=linkLocation;
+        cardText.innerText = `${festivalData[0]["index"][i]["strasse"]}, on ${festivalData[0]["index"][i]["von"]}, more info on `;
+
+        cardText.appendChild(eventLink);
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        card.appendChild(cardBody);
+        cardContainer.appendChild(card);
+        };
+    };
 };
 
 //#endregion
-//----->Table on the street-festivals.html page
+//----->display table function for the street-festivals.html page
 //#region
 
-// locating table header and table body
-let tableHeader = document.getElementById("table-header");
-let tableBody = document.getElementById("table-body");
+function displayTable () {
+    // locating table header and table body
+    let tableHeader = document.getElementById("table-header");
+    let tableBody = document.getElementById("table-body");
 
-//creating table contents from the data
-for (let n = 0; n < festivalData[0]["index"].length; n++) {
-    let tr = document.createElement("tr");
+    //validating if user on the festival page
+    if (tableBody != null) {
+        //creating table contents from the data
+        for (let n = 0; n < festivalData[0]["index"].length; n++) {
+            let tr = document.createElement("tr");
 
-    let td1 = document.createElement("td");
-    td1.innerHTML = festivalData[0]["index"][n]["rss_titel"];
-    let td2 = document.createElement("td");
-    td2.innerHTML = festivalData[0]["index"][n]["von"];
-    let td3 = document.createElement("td");
-    td3.innerHTML = festivalData[0]["index"][n]["bezirk"];
+            let td1 = document.createElement("td");
+            td1.innerHTML = festivalData[0]["index"][n]["rss_titel"];
+            let td2 = document.createElement("td");
+            td2.innerHTML = festivalData[0]["index"][n]["von"];
+            let td3 = document.createElement("td");
+            td3.innerHTML = festivalData[0]["index"][n]["bezirk"];
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tableBody.appendChild(tr);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tableBody.appendChild(tr);
+        }; 
+    }; 
 }; 
+
 
 //#endregion
 //
