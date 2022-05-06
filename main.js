e5fa72ebf4c264636a1d86ba8d799d01a7f4953c
podmar <marta.podziewska@gmail.com>
2022-05-06 16:21:13 +0200

    moved the check on which page the user is to the main controller function and removed some redundant comments

--- main.js
+++ main.js
@@ -37,11 +37,14 @@ fetch(url)
     .then(data => {
         // console.log(data);
         let festivalData = data.index; 
-        displayCards(filterFestivalsWithPhotos(festivalData));
-        displayTable(festivalData);
-        displayOptions(festivalData);
-        addEventListeners(festivalData);
-        searchByInput();
+            if (document.title.includes("Main")) {
+                displayCards(filterFestivalsWithPhotos(festivalData));
+            } else {
+                displayTable(festivalData);
+                displayOptions(festivalData);
+                addEventListeners(festivalData);
+                searchByInput();
+            };
         })
     .catch((err) => console.log(err)); 
 
@@ -100,47 +103,44 @@ function createEventLink (festivalData, i) {
 function displayCards (festivalData) {
     const cardContainer = document.getElementById("card-container");
 
-    //validating if user on the main page
-    if (cardContainer != null) {
-        //hide the spinner with d-none, as I want the content to be displayed at the top of the page.
-        const spinner = document.getElementById("spinner");
-        spinner.classList.add("d-none");
-        
-        //generating cards
-        for (let i = 0; i < 6; i++) {
+    //hide the spinner with d-none, as I want the content to be displayed at the top of the page.
+    const spinner = document.getElementById("spinner");
+    spinner.classList.add("d-none");
+    
+    //generating cards
+    for (let i = 0; i < 6; i++) {
 
-        const cardBox = document.createElement("div");
-        cardBox.classList.add("col-lg-4", "col-md-6", "p-2", "container");
-        
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.setAttribute("style", "height: 10rem");
-        
-        const photo = document.createElement("img");
-        photo.classList.add("card-img-top");
-        photo.setAttribute("src", `www.berlin.de${festivalData[i]["bild"]}`);
-        photo.setAttribute("alt", `photo of ${festivalData[i]["bezeichnung"]}`);
+    const cardBox = document.createElement("div");
+    cardBox.classList.add("col-lg-4", "col-md-6", "p-2", "container");
+    
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("style", "height: 10rem");
+    
+    const photo = document.createElement("img");
+    photo.classList.add("card-img-top");
+    photo.setAttribute("src", `www.berlin.de${festivalData[i]["bild"]}`);
+    photo.setAttribute("alt", `photo of ${festivalData[i]["bezeichnung"]}`);
 
-        const cardBody = document.createElement("div");
-        cardBody.setAttribute("class", "card-body");
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
 
-        const cardTitle = document.createElement("h5");
-        cardTitle.setAttribute("class", "card-title");
+    const cardTitle = document.createElement("h5");
+    cardTitle.setAttribute("class", "card-title");
 
-        const eventLink = createEventLink(festivalData, i);
-        
-        const cardText = document.createElement("p");
-        cardText.setAttribute("class", "card-text");
-        cardText.innerText = `${festivalData[i]["strasse"]} on ${festivalData[i]["von"]} until ${festivalData[i]["bis"]}.`;
-        
-        cardTitle.appendChild(eventLink);
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardText);
-        card.appendChild(photo);
-        card.appendChild(cardBody);
-        cardBox.appendChild(card);
-        cardContainer.appendChild(cardBox);
-        };
+    const eventLink = createEventLink(festivalData, i);
+    
+    const cardText = document.createElement("p");
+    cardText.setAttribute("class", "card-text");
+    cardText.innerText = `${festivalData[i]["strasse"]} on ${festivalData[i]["von"]} until ${festivalData[i]["bis"]}.`;
+    
+    cardTitle.appendChild(eventLink);
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    card.appendChild(photo);
+    card.appendChild(cardBody);
+    cardBox.appendChild(card);
+    cardContainer.appendChild(cardBox);
     };
 };
 
@@ -162,24 +162,20 @@ function displayOptions (festivalData) {
     //locating the select container for the options
     const select = document.getElementById("neighbourhoodSelect");
 
-    // console.log(select != null);
+    //creating options
+    let neighbourhoods = [];
+    festivalData.forEach(festival => {
+        if (!neighbourhoods.includes(festival.bezirk)) {
+            neighbourhoods.push(festival.bezirk);
+            let option = document.createElement("option");
+            option.innerText = festival.bezirk;
+            option.value = festival.bezirk;
 
-    //validating if user on the festival page
-    if (select != null) {
-        //creating options
-        let neighbourhoods = [];
-        festivalData.forEach(festival => {
-            if (!neighbourhoods.includes(festival.bezirk)) {
-                neighbourhoods.push(festival.bezirk);
-                let option = document.createElement("option");
-                option.innerText = festival.bezirk;
-                option.value = festival.bezirk;
-
-                select.appendChild(option);
-            }; 
-        });
-    };
+            select.appendChild(option);
+        }; 
+    });
 };
+
 //#endregion
 //-> functions to filter by neighbourhood / month + controller
 //#region
@@ -187,7 +183,7 @@ function displayOptions (festivalData) {
 //-> filter by neighbourhood select
 function filterByNeighbourhood (festivalData) {
     const neighbourhood = document.getElementById("neighbourhoodSelect").value;
-    console.log(neighbourhood);
+    // console.log(neighbourhood);
     let filteredByNeighbourhood = festivalData.filter(festival => 
         festival.bezirk === neighbourhood || neighbourhood === "all");
     displayTable(filteredByNeighbourhood);
@@ -235,25 +231,22 @@ function filterByMonth (festivalData) {
 
 //-> without a main filter controller function
 function addEventListeners (festivalData) {
-    //validating if user on the festival page
-    if (document.getElementById("table-header") != null) {
-        document.getElementById("neighbourhoodSelect")
-            .addEventListener("change", function event () {
-                if (document.getElementById("monthInput").value === "") {
-                    filterByNeighbourhood(festivalData); 
-                } else {
-                    filterByNeighbourhood(filterByMonth(festivalData))
-                };
-            });
-        document.getElementById("monthInput")
-            .addEventListener("change", function event () {
-                if (document.getElementById("neighbourhoodSelect").value === "all") {
-                    filterByMonth(festivalData);
-                } else {
-                    filterByMonth(filterByNeighbourhood(festivalData));
-                };
-            });
-    };
+    document.getElementById("neighbourhoodSelect")
+        .addEventListener("change", function event () {
+            if (document.getElementById("monthInput").value === "") {
+                filterByNeighbourhood(festivalData); 
+            } else {
+                filterByNeighbourhood(filterByMonth(festivalData))
+            };
+        });
+    document.getElementById("monthInput")
+        .addEventListener("change", function event () {
+            if (document.getElementById("neighbourhoodSelect").value === "all") {
+                filterByMonth(festivalData);
+            } else {
+                filterByMonth(filterByNeighbourhood(festivalData));
+            };
+        });
 };
 
 //#endregion
@@ -264,53 +257,53 @@ function displayTable (festivalData) {
     // locating the table body
     const tableBody = document.getElementById("table-body");
 
-    //validating if user on the festival page
-    if (tableBody != null) {
-        //hide the spinner with d-none, as I want the content to be displayed at the top of the page.
-        const spinner = document.getElementById("spinner");
-        spinner.classList.add("d-none");
-        //display table
-        const contentToDisplay = document.getElementById("content-to-display");
-        contentToDisplay.classList.remove("invisible");
-        //setting the table content to empty - needed while updating filters
-        clearDynamicSearch();
-        //creating table contents from the data
-        for (let n = 0; n < festivalData.length; n++) {
-            const tr = document.createElement("tr");
+    //hide the spinner with d-none, as I want the content to be displayed at the top of the page.
+    const spinner = document.getElementById("spinner");
+    spinner.classList.add("d-none");
 
-            const td1 = document.createElement("td");
-            const eventLink = createEventLink(festivalData, n);
-            // td1.innerHTML = festivalData[n]["bezeichnung"];
+    //display table
+    const contentToDisplay = document.getElementById("content-to-display");
+    contentToDisplay.classList.remove("invisible");
 
-            const td2 = document.createElement("td");
-            const date = new Date(festivalData[n]["von"]).toLocaleDateString("en-GB", {
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-              });
-            td2.innerHTML = date;
+    //setting the table content to empty - needed while updating filters
+    clearDynamicSearch();
 
-            const td3 = document.createElement("td");
-            td3.innerHTML = festivalData[n]["bezirk"];
+    //creating table contents from the data
+    for (let n = 0; n < festivalData.length; n++) {
+        const tr = document.createElement("tr");
 
-            const td4 = document.createElement("td");
-            const festivalDetailButton = createFestivalDetailButton(festivalData[n]);
-            const festivalModal = createModal(festivalData[n]);
+        const td1 = document.createElement("td");
+        const eventLink = createEventLink(festivalData, n);
 
-            document.body.appendChild(festivalModal);
-            tr.appendChild(td1);
-            td1.appendChild(eventLink);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            td4.appendChild(festivalDetailButton);
-            tr.appendChild(td4);
-            tableBody.appendChild(tr);
-        }; 
-        let numberOfResults = tableBody.childElementCount;
-        displayNoResultsNotification(numberOfResults);
+        const td2 = document.createElement("td");
+        const date = new Date(festivalData[n]["von"]).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            });
+        td2.innerHTML = date;
+
+        const td3 = document.createElement("td");
+        td3.innerHTML = festivalData[n]["bezirk"];
+
+        const td4 = document.createElement("td");
+        const festivalDetailButton = createFestivalDetailButton(festivalData[n]);
+        const festivalModal = createModal(festivalData[n]);
+
+        document.body.appendChild(festivalModal);
+        tr.appendChild(td1);
+        td1.appendChild(eventLink);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        td4.appendChild(festivalDetailButton);
+        tr.appendChild(td4);
+        tableBody.appendChild(tr);
     }; 
 
+    let numberOfResults = tableBody.childElementCount;
+    displayNoResultsNotification(numberOfResults);
 }; 
+
 //-> function for no results notification
 function displayNoResultsNotification (numberOfResults) {
         const tableHeader = document.getElementById("table-header");
